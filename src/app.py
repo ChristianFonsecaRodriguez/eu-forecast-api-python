954b1eac554b89f7c911e173618d31eb55a3ed4b
ChristianFonsecaRodriguez <cfonsecar@uni.pe>
2024-12-07 01:19:18 -0500

    api-submit

--- src/app.py
+++ src/app.py
@@ -88,5 +88,5 @@ async def predict(input: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
